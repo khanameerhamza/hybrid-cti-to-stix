fac31f1a6f7f8bc3841b516c38d831cd35907d3a
Ameer Hamza Khan <khanhamza2572@gmail.com>
2025-12-14 22:31:37 +0500

    Update LLM_check.py

--- LLM_check.py
+++ LLM_check.py
@@ -1,8 +1,9 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyAwBX9tFcnUnj-CjBmVvBtzZMUJYgV2SD8")
+client = genai.Client(api_key="YOUR API KEY")
 chat = client.chats.create(model="gemini-2.5-flash")
 
 # Optional: add system instruction
 response = chat.send_message("You are a STIX validation expert. Validate this JSON versus the document.")
 print(response.text)
+
